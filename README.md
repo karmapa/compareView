1b626b824fc435ced7fd5f66a36b962292416fb7
zhi-xuan <rickie120243@gmail.com>
2016-05-27 11:37:24 +0800

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# pageToPageComparisonUI
+page to page comparison UI
+
+##Usage
+####build environment
+```
+npm install
+```
+
+
+####pack
+pack files and export to "dist" folder.
+```
+npm install webpack --global
+webpack
+```
+
+
+####webpack-dev-server
+demo at http://localhost:8080
+```
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+
+  devtool: 'eval',
+
+  entry: {
+    javascript: [
+      './src/index'
+    ],
+    html: './index.html'
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ],
+
+  resolve: {
+    extensions: ['', '.js']
+  },
+
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        configFile: './.eslintrc',
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel'],
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader'},
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[name].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: "file-loader?name=[name].[ext]",
+      }
+    ]
+  }
+
+};
