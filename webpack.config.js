f0248284b53c4756f371068fcb3590b42028affa
zhi-xuan <rickie120243@gmail.com>
2016-05-31 17:36:35 +0800

    add imagezoomer from adarsha2016.5

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,10 @@ module.exports = {
       },
       { test: /\.css$/, loader: 'style-loader!css-loader'},
       {
+        test: /\.scss$/,
+        loader: 'style!css!sass?outputStyle=expanded'
+      },
+      {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
           'file?hash=sha512&digest=hex&name=[name].[ext]',
