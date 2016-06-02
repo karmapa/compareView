866e613247c5a839a35d2e2bf4e13b39ea49a470
zhi-xuan <rickie120243@gmail.com>
2016-06-02 15:45:36 +0800

    connect TabPane & add tabKey

--- src/containers/TabPane1.js
+++ src/containers/TabPane1.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import ImageZoomer from '../components/ImageZoomer/ImageZoomer.js';
+import {connect} from 'react-redux';
 
-class TabPane1 extends Component {
+class TabPane1Component extends Component {
   render() {
     return (
       <div id="tabPane1">
-        <ImageZoomer src="http://res.cloudinary.com/www-dharma-treasure-org/image/upload/lijiang/001/001-007a.jpg"/>
+        <ImageZoomer tabKey="1" src="http://res.cloudinary.com/www-dharma-treasure-org/image/upload/lijiang/001/001-007a.jpg"/>
         <div id="pbText">
           དེ་ནས་བྲམ་ཟེ་དེས་བྲམ་ཟེའི་ཁྱེའུ་དེ་ལ་འོངས་པ་ལེགས་སོ་ཞེས་བྱ་བའི་ཚིག་གིས་ཀུན་དུwfwefwefwfwe
           དེ་ནས་བྲམ་ཟེ་དེས་བྲམ་ཟེའི་ཁྱེའུ་དེ་ལ་འོངས་པ་ལེགས་སོ་ཞེས་བྱ་བའི་ཚིག་གིས་ཀུན་དུ
@@ -14,5 +15,13 @@ class TabPane1 extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    state: state.comparison
+  };
+};
+
+const TabPane1 = connect(mapStateToProps)(TabPane1Component);
 
 export default TabPane1;
--- src/containers/TabPane2.js
+++ src/containers/TabPane2.js
@@ -4,14 +4,13 @@ import {connect} from 'react-redux';
 
 class TabPane2Component extends Component {
   shouldComponentUpdate(nextProps, nextState) {
-    console.log(nextProps.state.tabKey ,this.props.state.tabKey);
     return (nextProps.state.tabKey !== this.props.state.tabKey);
   }
 
   render() {
     return (
       <div id="tabPane2">
-        <ImageZoomer src="http://res.cloudinary.com/www-dharma-treasure-org/image/upload/lijiang/001/001-005a.jpg"/>
+        <ImageZoomer tabKey="2" src="http://res.cloudinary.com/www-dharma-treasure-org/image/upload/lijiang/001/001-005a.jpg"/>
         <div id="pbText">
           དེ་ནས་བྲམ་ཟེ་དེས་བྲམ་ཟེའི་ཁྱེའུ་དེ་ལ་འོངས་པ་ལེགས་སོ་ཞེས་བྱ་བའི་ཚིག་གིས་ཀུན་དུwfwefwefwfwe
           དེ་ནས་བྲམ་ཟེ་དེས་བྲམ་ཟེའི་ཁྱེའུ་དེ་ལ་འོངས་པ་ལེགས་སོ་ཞེས་བྱ་བའི་ཚིག་གིས་ཀུན་དུ
