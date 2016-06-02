6eda58677112ae3b0ac261164f09c5ef98c318f2
zhi-xuan <rickie120243@gmail.com>
2016-06-02 15:31:09 +0800

    add test text

--- src/containers/TabPane1.js
+++ src/containers/TabPane1.js
@@ -6,6 +6,10 @@ class TabPane1 extends Component {
     return (
       <div id="tabPane1">
         <ImageZoomer src="http://res.cloudinary.com/www-dharma-treasure-org/image/upload/lijiang/001/001-007a.jpg"/>
+        <div id="pbText">
+          དེ་ནས་བྲམ་ཟེ་དེས་བྲམ་ཟེའི་ཁྱེའུ་དེ་ལ་འོངས་པ་ལེགས་སོ་ཞེས་བྱ་བའི་ཚིག་གིས་ཀུན་དུwfwefwefwfwe
+          དེ་ནས་བྲམ་ཟེ་དེས་བྲམ་ཟེའི་ཁྱེའུ་དེ་ལ་འོངས་པ་ལེགས་སོ་ཞེས་བྱ་བའི་ཚིག་གིས་ཀུན་དུ
+        </div>
       </div>
     );
   }
