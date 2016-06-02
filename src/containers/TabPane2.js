f0f8c79d66dc3a0d3cc2293d7f989955e868e48f
zhi-xuan <rickie120243@gmail.com>
2016-06-02 15:47:14 +0800

    fix tabKey id

--- src/containers/TabPane2.js
+++ src/containers/TabPane2.js
@@ -10,7 +10,7 @@ class TabPane2Component extends Component {
   render() {
     return (
       <div id="tabPane2">
-        <ImageZoomer tabKey="2" src="http://res.cloudinary.com/www-dharma-treasure-org/image/upload/lijiang/001/001-005a.jpg"/>
+        <ImageZoomer tabKey="tab2" src="http://res.cloudinary.com/www-dharma-treasure-org/image/upload/lijiang/001/001-005a.jpg"/>
         <div id="pbText">
           དེ་ནས་བྲམ་ཟེ་དེས་བྲམ་ཟེའི་ཁྱེའུ་དེ་ལ་འོངས་པ་ལེགས་སོ་ཞེས་བྱ་བའི་ཚིག་གིས་ཀུན་དུwfwefwefwfwe
           དེ་ནས་བྲམ་ཟེ་དེས་བྲམ་ཟེའི་ཁྱེའུ་དེ་ལ་འོངས་པ་ལེགས་སོ་ཞེས་བྱ་བའི་ཚིག་གིས་ཀུན་དུ
