77d674147bdde3cc7e96cc342f1690e5b016bfd9
zhi-xuan <rickie120243@gmail.com>
2016-06-29 18:10:35 +0800

    add page input

--- src/containers/PageControl.js
+++ src/containers/PageControl.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import store from '../store/store.js';
-import {pagePre, pageNext} from '../reducers/comparison.js';
+import {pagePre, pageNext, pageInput, pageKeyPress} from '../reducers/comparison.js';
 import {connect} from 'react-redux';
 
 class PageControlComponent extends Component {
@@ -12,9 +12,18 @@ class PageControlComponent extends Component {
     store.dispatch(pageNext());
   }
 
+  onPageKeyPress(event) {
+    store.dispatch(pageKeyPress(event.key));
+  }
+
+  handleChange(event) {
+    store.dispatch(pageInput(event.target.value));
+  }
+
   render() {
     return (
       <span>
+        <input id="pageInput" className="" value={this.props.state.pageInput} onKeyPress={this.onPageKeyPress} onChange={this.handleChange}></input>
         <button id="pagePre" onClick={this.onPagePre}></button>
         <button id="pageNext" onClick={this.onPageNext}></button>
       </span>
--- src/reducers/comparison.js
+++ src/reducers/comparison.js
@@ -9,10 +9,12 @@ const oldPages = Object.keys(text1[textName1]);
 const initialState = {
   comparePbList: comparePbList,
   tabKey: 1,
+  pageInput: oldPages[0],
   currentPage1: oldPages[0],
   currentPage2: comparePbList[oldPages[0]][0],
   pageNumber: 0,
   pages: oldPages,
+  wrongPageInput: false,
   warnNext: false,
   warnPre: false,
   textName1: textName1,
@@ -35,17 +37,21 @@ const reducer = (state = initialState, action) => {
           wrongPageInput: false
         };
       } else {
+        let obj = {};
+        let pageInput = 'currentPage' + state.tabKey;
+        obj.currentPage1 = state.pages[state.pageNumber + 1];
+        obj.currentPage2 = state.comparePbList[state.pages[state.pageNumber + 1]][0];
         return {
           ...state,
           warnPre: false,
           wrongPageInput: false,
           pageNumber: state.pageNumber + 1,
-          currentPage1: state.pages[state.pageNumber + 1],
-          currentPage2: state.comparePbList[state.pages[state.pageNumber + 1]][0],
+          currentPage1: obj.currentPage1,
+          currentPage2: obj.currentPage2,
           comparePos: [state.comparePbList[state.pages[state.pageNumber + 1]][1], state.comparePbList[state.pages[state.pageNumber + 1]][2]],
           text1: text1[textName1][state.pages[state.pageNumber + 1]],
           text2: text2[textName2][state.comparePbList[state.pages[state.pageNumber + 1]][0]],
-          pageInput: state.pages[state.pageNumber + 1]
+          pageInput: obj[pageInput]
         };
       }
     case 'PAGEPRE':
@@ -56,17 +62,63 @@ const reducer = (state = initialState, action) => {
           wrongPageInput: false
         };
       } else {
+        let obj = {};
+        let pageInput = 'currentPage' + state.tabKey;
+        obj.currentPage1 = state.pages[state.pageNumber - 1];
+        obj.currentPage2 = state.comparePbList[state.pages[state.pageNumber - 1]][0];
         return {
           ...state,
           warnNext: false,
           wrongPageInput: false,
           pageNumber: state.pageNumber - 1,
-          currentPage1: state.pages[state.pageNumber - 1],
-          currentPage2: state.comparePbList[state.pages[state.pageNumber - 1]][0],
+          currentPage1: obj.currentPage1,
+          currentPage2: obj.currentPage2,
           comparePos: [state.comparePbList[state.pages[state.pageNumber - 1]][1], state.comparePbList[state.pages[state.pageNumber - 1]][2]],
           text1: text1[textName1][state.pages[state.pageNumber - 1]],
           text2: text2[textName2][state.comparePbList[state.pages[state.pageNumber - 1]][0]],
-          pageInput: state.pages[state.pageNumber - 1]
+          pageInput: obj[pageInput]
+        };
+      }
+    case 'PAGEINPUT':
+      return {
+        ...state,
+        pageInput: action.input,
+        wrongPageInput: false
+      };
+    case 'PAGEKEYPRESS':
+      if (('Enter' === action.key) && (state.pages.some(pb => state.pageInput === pb)) && (1 === state.tabKey)) {
+        let pageNumber = state.pages.indexOf(state.pageInput);
+        return {
+          ...state,
+          pageNumber: pageNumber,
+          text1: text1[textName1][state.pages[pageNumber]],
+          text2: text2[textName2][state.comparePbList[state.pages[pageNumber]][0]],
+          currentPage1: state.pageInput,
+          currentPage2: state.comparePbList[state.pageInput][0],
+          comparePos: [comparePbList[state.pageInput][1], comparePbList[state.pageInput][2]],
+          wrongPageInput: false
+        };
+      } else if (('Enter' === action.key) && (1 !== state.tabKey)) {
+        for (let key in state.comparePbList) {
+          if (state.pageInput === state.comparePbList[key][state.tabKey - 2]) {
+            let pageNumber = state.pages.indexOf(key);
+            return {
+              ...state,
+              pageNumber: pageNumber,
+              text1: text1[textName1][key],
+              text2: text2[textName2][state.pageInput],
+              currentPage1: key,
+              currentPage2: state.pageInput,
+              comparePos: [comparePbList[key][1], comparePbList[key][2]],
+              wrongPageInput: false
+            };
+          }
+        }
+      } else {
+        return {
+          ...state,
+          pageInput: state.pageInput,
+          wrongPageInput: true
         };
       }
     case 'THEMESWITCH':
@@ -75,9 +127,11 @@ const reducer = (state = initialState, action) => {
         themeStyle: 'theme' + action.style
       };
     case 'TABSWITCH':
+      let currentPage = 'currentPage' + action.key;
       return {
         ...state,
-        tabKey: action.key
+        tabKey: action.key,
+        pageInput: state[currentPage]
       };
     default:
       return state;
@@ -95,6 +149,20 @@ export function pageNext() {
 export function pagePre() {
   return {
     type: 'PAGEPRE'
+  };
+}
+
+export function pageInput(input) {
+  return {
+    type: 'PAGEINPUT',
+    input: input
+  };
+}
+
+export function pageKeyPress(key) {
+  return {
+    type: 'PAGEKEYPRESS',
+    key: key
   };
 }
 
